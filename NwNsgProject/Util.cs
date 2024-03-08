88fbdc043f0e5b86f144922b004ef9644dd53720
Kodai Ishikawa <kodai.ishikawa@dynatrace.com>
2024-03-08 12:10:56 -0500

    Revert

--- NwNsgProject/Util.cs
+++ NwNsgProject/Util.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
-using Azure.Storage.Blobs;
+using Microsoft.Azure.Storage.Blob;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
